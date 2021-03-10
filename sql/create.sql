126ed04cec18830ea57b1cfd08599cd36cad9607
Anthony Pineau <anthonypineau85@gmail.com>
2021-03-10 13:36:08 +0200

    SQL

--- sql/create.sql
+++ sql/create.sql
@@ -1,28 +1,28 @@
-DROP TABLE IF EXISTS `Item_class`;
-DROP TABLE IF EXISTS `Item_subclass`;
-DROP TABLE IF EXISTS `Item`;
-DROP TABLE IF EXISTS `Pet`;
-DROP TABLE IF EXISTS `Class`;
-DROP TABLE IF EXISTS `Race`;
-DROP TABLE IF EXISTS `Profession`;
-DROP TABLE IF EXISTS `Title`;
+DROP TABLE IF EXISTS `Items`;
+DROP TABLE IF EXISTS `Item_subclasses`;
+DROP TABLE IF EXISTS `Item_classes`;
+DROP TABLE IF EXISTS `Pets`;
+DROP TABLE IF EXISTS `Classes`;
+DROP TABLE IF EXISTS `Races`;
+DROP TABLE IF EXISTS `Professions`;
+DROP TABLE IF EXISTS `Titles`;
 
-create table Item_class
+create table Item_classes
 (id int not null auto_increment,
 name varchar(45) not null,
-constraint pk_Item_class primary key(id))
+constraint pk_Item_classes primary key(id))
 ENGINE=INNODB;
 
-create table Item_subclass
+create table Item_subclasses
 (id int not null auto_increment,
 name varchar(45) not null,
 item_class int not null,
-constraint pk_Item_subclass primary key(id),
-constraint fk_Item_subclass foreign key(item_class) references Item_class(id) 
+constraint pk_Item_subclasses primary key(id),
+constraint fk_Item_subclasses foreign key(item_class) references Item_classes(id) 
 ON DELETE CASCADE ON UPDATE CASCADE)
 ENGINE=INNODB;
 
-create table Item
+create table Items
 (id int not null auto_increment, 
 name varchar(45) not null,
 level int not null,
@@ -33,14 +33,14 @@ is_stackable bit(1) not null,
 media varchar(250),
 item_class int not null,
 item_subclass int not null,
-constraint pk_Item primary key(id),
-constraint fk1_Item foreign key(item_class) references Item_class(id) 
+constraint pk_Items primary key(id),
+constraint fk1_Items foreign key(item_class) references Item_classes(id) 
 ON DELETE CASCADE ON UPDATE CASCADE,
-constraint fk2_Item foreign key(item_subclass) references Item_subclass(id) 
+constraint fk2_Items foreign key(item_subclass) references Item_subclasses(id) 
 ON DELETE CASCADE ON UPDATE CASCADE)
 ENGINE=INNODB;
 
-create table Pet
+create table Pets
 (id int not null auto_increment,
 name varchar(45) not null,
 description text not null,
@@ -50,42 +50,40 @@ is_battlepet bit(1) not null,
 is_alliance_only bit(1) not null,
 is_horde_only bit(1) not null,
 is_tameable bit(1) not null,
-media varchar(250) not null,
-constraint pk_Pet primary key(id))
+display varchar(250) not null,
+icon varchar(250) not null,
+constraint pk_Pets primary key(id))
 ENGINE=INNODB;
 
-create table Class
+create table Classes
 (id int not null auto_increment,
 name varchar(45) not null,
 female_name varchar(45) not null,
 male_name varchar(45) not null,
 media varchar(250) not null,
-constraint pk_Class primary key(id))
+constraint pk_Classes primary key(id))
 ENGINE=INNODB;
 
-create table Race
+create table Races
 (id int not null auto_increment,
 name varchar(45) not null,
-
 female_name varchar(45) not null,
 male_name varchar(45) not null,
-media varchar(250) not null,
-constraint pk_Race primary key(id))
+constraint pk_Races primary key(id))
 ENGINE=INNODB;
 
-create table Profession
+create table Professions
 (id int not null auto_increment,
 name varchar(45) not null,
 description text not null,
-type varchar(45) not null,
 media varchar(250) not null,
-constraint pk_Profession primary key(id))
+constraint pk_Professions primary key(id))
 ENGINE=INNODB;
 
-create table Title
+create table Titles
 (id int not null auto_increment,
 name varchar(45) not null,
 female_name varchar(45) not null,
 male_name varchar(45) not null,
-constraint pk_Title primary key(id))
+constraint pk_Titles primary key(id))
 ENGINE=INNODB;
